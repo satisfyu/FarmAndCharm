0856d2756f86e1b3e0c10552bb4a1ce4d47509da
satisfyu <104036234+satisfyu@users.noreply.github.com>
2024-04-16 21:06:26 +0200

    Fixed: Plow wheel rotation

--- common/src/main/java/satisfy/farm_and_charm/client/model/PlowModel.java
+++ common/src/main/java/satisfy/farm_and_charm/client/model/PlowModel.java
@@ -27,24 +27,25 @@ public class PlowModel<T extends CartEntity> extends EntityModel<T> {
         PartDefinition partdefinition = meshdefinition.getRoot();
 
         PartDefinition cart = partdefinition.addOrReplaceChild("cart", CubeListBuilder.create().texOffs(153, 44).addBox(-11.0F, 0.5F, 4.9167F, 22.0F, 6.0F, 6.0F, new CubeDeformation(0.0F))
-                .texOffs(159, 8).addBox(-3.0F, 0.5F, -27.0833F, 4.0F, 4.0F, 32.0F, new CubeDeformation(0.0F))
+                .texOffs(159, 8).addBox(-2.0F, 0.5F, -27.0833F, 4.0F, 4.0F, 32.0F, new CubeDeformation(0.0F))
                 .texOffs(88, 0).addBox(5.0F, 2.5F, 10.9167F, 3.0F, 3.0F, 18.0F, new CubeDeformation(0.0F))
                 .texOffs(88, 0).addBox(-8.0F, 2.5F, 10.9167F, 3.0F, 3.0F, 18.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 12.5F, 10.0833F));
 
-        PartDefinition plow_r1 = cart.addOrReplaceChild("plow_r1", CubeListBuilder.create().texOffs(173, 61).addBox(-25.0F, 7.0F, -7.0F, 16.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -3.0F, -12.0F, 0.0F, -0.7854F, 0.0F));
+        PartDefinition plow_r1 = cart.addOrReplaceChild("plow_r1", CubeListBuilder.create().texOffs(173, 61).addBox(-12.0F, -4.0F, 4.0F, 16.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.4142F, 8.0F, -28.9706F, 0.0F, -0.7854F, 0.0F));
 
-        PartDefinition plow_r2 = cart.addOrReplaceChild("plow_r2", CubeListBuilder.create().texOffs(173, 61).mirror().addBox(7.0F, 7.0F, -9.0F, 16.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(0.0F, -3.0F, -12.0F, 0.0F, 0.7854F, 0.0F));
+        PartDefinition plow_r2 = cart.addOrReplaceChild("plow_r2", CubeListBuilder.create().texOffs(173, 61).addBox(-4.0F, -4.0F, 4.0F, 16.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.4142F, 8.0F, -28.9706F, 0.0F, 0.7854F, 0.0F));
 
-        PartDefinition plow_r3 = cart.addOrReplaceChild("plow_r3", CubeListBuilder.create().texOffs(173, 61).addBox(-25.0F, 7.0F, -7.0F, 16.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -3.0F, 4.0F, 0.0F, -0.7854F, 0.0F));
+        PartDefinition plow_r3 = cart.addOrReplaceChild("plow_r3", CubeListBuilder.create().texOffs(173, 61).addBox(-12.0F, -4.0F, 4.0F, 16.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.4142F, 8.0F, -12.9706F, 0.0F, -0.7854F, 0.0F));
 
-        PartDefinition plow_r4 = cart.addOrReplaceChild("plow_r4", CubeListBuilder.create().texOffs(173, 61).mirror().addBox(7.0F, 7.0F, -9.0F, 16.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(0.0F, -3.0F, 4.0F, 0.0F, 0.7854F, 0.0F));
+        PartDefinition plow_r4 = cart.addOrReplaceChild("plow_r4", CubeListBuilder.create().texOffs(173, 61).addBox(-4.0F, -4.0F, 4.0F, 16.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.4142F, 8.0F, -12.9706F, 0.0F, 0.7854F, 0.0F));
 
-        PartDefinition right_wheel = partdefinition.addOrReplaceChild("right_wheel", CubeListBuilder.create().texOffs(76, 43).addBox(-3.0F, -16.0F, 10.0F, 3.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(14.0F, 24.0F, 0.0F));
+        PartDefinition right_wheel = partdefinition.addOrReplaceChild("right_wheel", CubeListBuilder.create().texOffs(76, 43).addBox(-1.5F, -8.0F, -8.0F, 3.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(12.5F, 16.0F, 18.0F));
 
-        PartDefinition left_wheel = partdefinition.addOrReplaceChild("left_wheel", CubeListBuilder.create().texOffs(76, 43).mirror().addBox(-14.0F, -16.0F, 10.0F, 3.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.0F, 24.0F, 0.0F));
+        PartDefinition left_wheel = partdefinition.addOrReplaceChild("left_wheel", CubeListBuilder.create().texOffs(76, 43).addBox(-1.5F, -8.0F, -8.0F, 3.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(-12.5F, 16.0F, 18.0F));
 
         return LayerDefinition.create(meshdefinition, 256, 256);
     }
+
     @Override
     public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
         this.cart.xRot = entity.balance();
