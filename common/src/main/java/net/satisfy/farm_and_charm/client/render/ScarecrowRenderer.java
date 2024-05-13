96845122f924ad76acb826831189d73a99bcf9b7
satisfyu <104036234+satisfyu@users.noreply.github.com>
2024-05-13 22:47:06 +0200

    Update ScarecrowRenderer.java

--- common/src/main/java/net/satisfy/farm_and_charm/client/render/ScarecrowRenderer.java
+++ common/src/main/java/net/satisfy/farm_and_charm/client/render/ScarecrowRenderer.java
@@ -16,11 +16,14 @@ import net.satisfy.farm_and_charm.block.ScarecrowBlock;
 import net.satisfy.farm_and_charm.block.entity.ScarecrowBlockEntity;
 import net.satisfy.farm_and_charm.client.model.ScarecrowModel;
 import org.joml.Quaternionf;
+
 public class ScarecrowRenderer implements BlockEntityRenderer<ScarecrowBlockEntity> {
 
     private static final ResourceLocation TEXTURE = new ResourceLocation(FarmAndCharm.MOD_ID, "textures/entity/scarecrow.png");
     private final ModelPart scarecrow;
     private final ModelPart post;
+    private float swayAngle = 0.0F;
+    private boolean swayForward = true;
 
     public ScarecrowRenderer(BlockEntityRendererProvider.Context context) {
         ModelPart root = context.bakeLayer(ScarecrowModel.LAYER_LOCATION);
@@ -28,28 +31,38 @@ public class ScarecrowRenderer implements BlockEntityRenderer<ScarecrowBlockEnti
         this.post = root.getChild("post");
     }
 
+    private float updateSwayAngle(ScarecrowBlockEntity blockEntity) {
+        float swayIncrement = 0.01f;
+        if (swayForward) {
+            swayAngle += swayIncrement;
+            if (swayAngle >= 2.0f) {
+                swayForward = false;
+            }
+        } else {
+            swayAngle -= swayIncrement;
+            if (swayAngle <= -2.0f) {
+                swayForward = true;
+            }
+        }
+        return swayAngle;
+    }
+
     @Override
     public void render(ScarecrowBlockEntity blockEntity, float partialTicks, PoseStack poseStack, MultiBufferSource bufferSource, int combinedLight, int combinedOverlay) {
+        VertexConsumer vertexConsumer = bufferSource.getBuffer(RenderType.entityCutoutNoCull(TEXTURE));
         Direction direction = blockEntity.getBlockState().getValue(ScarecrowBlock.FACING);
         float rotationDegrees = -direction.toYRot() + 180;
+        final var angle = updateSwayAngle(blockEntity);
 
-        if (blockEntity.getLevel() != null && blockEntity.getLevel().isLoaded(blockEntity.getBlockPos())) {
-            long gameTime = blockEntity.getLevel().getGameTime();
-            boolean isStormy = blockEntity.getLevel().isThundering() || blockEntity.getLevel().isRaining();
-            float speedMultiplier = isStormy ? 0.1f : 0.05f;
-            float angleDegrees = isStormy ? 2.0f : Mth.sin((gameTime + partialTicks) * speedMultiplier) * 1;
+        poseStack.pushPose();
+        poseStack.translate(0.5, 0, 0.5);
+        poseStack.mulPose(new Quaternionf().rotateY((float) Math.toRadians(rotationDegrees)));
+        poseStack.mulPose(new Quaternionf().rotateX((float) Math.toRadians(angle)));
+        poseStack.translate(-0.5, 0, -0.5);
 
-            poseStack.pushPose();
-            poseStack.translate(0.5, 0, 0.5);
-            poseStack.mulPose(new Quaternionf().rotateY((float) Math.toRadians(rotationDegrees)));
-            poseStack.mulPose(new Quaternionf().rotateX((float) Math.toRadians(angleDegrees)));
-            poseStack.translate(-0.5, 0, -0.5);
+        scarecrow.render(poseStack, vertexConsumer, combinedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+        post.render(poseStack, vertexConsumer, combinedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
 
-            VertexConsumer vertexConsumer = bufferSource.getBuffer(RenderType.entityCutoutNoCull(TEXTURE));
-            scarecrow.render(poseStack, vertexConsumer, combinedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
-            post.render(poseStack, vertexConsumer, combinedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
-
-            poseStack.popPose();
-        }
+        poseStack.popPose();
     }
 }
