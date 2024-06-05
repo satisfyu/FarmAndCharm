677fe21ba7775779e57c50386a884c3f93d47b25
satisfyu <104036234+satisfyu@users.noreply.github.com>
2024-06-05 22:17:59 +0200

    Added: Farmland tag

--- common/src/main/java/net/satisfy/farm_and_charm/registry/TagRegistry.java
+++ common/src/main/java/net/satisfy/farm_and_charm/registry/TagRegistry.java
@@ -6,7 +6,9 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.satisfy.farm_and_charm.util.FarmAndCharmIdentifier;
 
+@SuppressWarnings("unused")
 public class TagRegistry {
+    public static final TagKey<Block> FARMLAND = TagKey.create(Registries.BLOCK, new FarmAndCharmIdentifier("farmland"));
     public static final TagKey<Block> SUPPRESS_CAMPFIRE_SMOKE_PARTICLES = TagKey.create(Registries.BLOCK, new FarmAndCharmIdentifier("suppress_campfire_smoke_particles"));
     public static final TagKey<Block> COOKING_POTS = TagKey.create(Registries.BLOCK, new FarmAndCharmIdentifier("cooking_pots"));
     public static final TagKey<Block> ALLOWS_COOKING = TagKey.create(Registries.BLOCK, new FarmAndCharmIdentifier("allows_cooking"));
