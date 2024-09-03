fb2a803b2f08efc7e23c7394e9cc74c63cac7997
satisfy <laube_marco@yahoo.de>
2024-09-03 10:43:05 +0200

    Update FeastEffect.java

--- common/src/main/java/net/satisfy/farm_and_charm/effect/FeastEffect.java
+++ common/src/main/java/net/satisfy/farm_and_charm/effect/FeastEffect.java
@@ -23,16 +23,17 @@ public class FeastEffect extends MobEffect {
 
             long worldTime = player.getCommandSenderWorld().getDayTime() % 24000;
 
-            if (foodData.needsFood() || player.hasEffect(MobEffects.REGENERATION) || foodData.getSaturationLevel() <= 0f) {
-                if (!shouldHeal) {
-                    if (foodData.getFoodLevel() > 3 || foodData.getSaturationLevel() <= 0f) {
+            if (!shouldHeal) {
+                if (foodData.needsFood()) {
+                    if (foodData.getFoodLevel() > 3) {
                         foodData.setFoodLevel(Math.max(foodData.getFoodLevel() - 1, 3));
                     }
                     foodData.addExhaustion(-4.0f);
                 }
-                if (worldTime >= 10000 && worldTime <= 13000) {
-                    foodData.eat(6, 0.6f);
-                }
+            }
+
+            if (worldTime >= 10000 && worldTime <= 13000) {
+                foodData.eat(6, 0.6f);
             }
         }
     }
