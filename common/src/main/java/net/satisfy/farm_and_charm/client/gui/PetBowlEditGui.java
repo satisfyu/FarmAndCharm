d0e5214d21af2604785d3c1a2f360985db8fec41
amiralimollaei <me.amiralimollaei@gmail.com>
2026-07-18 20:06:31 +0330

    fix: don't render background twice

--- common/src/main/java/net/satisfy/farm_and_charm/client/gui/PetBowlEditGui.java
+++ common/src/main/java/net/satisfy/farm_and_charm/client/gui/PetBowlEditGui.java
@@ -44,7 +44,7 @@ public class PetBowlEditGui extends Screen {
 
     @Override
     public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
-        this.renderBackground(graphics, mouseX, mouseY, partialTick);
+        //this.renderBackground(graphics, mouseX, mouseY, partialTick);
         super.render(graphics, mouseX, mouseY, partialTick);
         graphics.drawCenteredString(this.font, this.title, this.width / 2, 20, 16777215);
     }
